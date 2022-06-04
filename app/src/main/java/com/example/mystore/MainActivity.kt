df0455635f99a2fadea8c60451acee8d0fa0c7b8
maciej.heintze <maciej.heintze@consultant.volvo.com>
2022-06-04 13:22:34 +0200

    navigation added

--- app/src/main/java/com/example/mystore/MainActivity.kt
+++ app/src/main/java/com/example/mystore/MainActivity.kt
@@ -10,34 +10,33 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import com.example.mystore.databinding.MainContentBinding
 import com.example.mystore.ui.theme.MyStoreTheme
 
 class MainActivity : ComponentActivity() {
+
+    private lateinit var binding: MainContentBinding
+    private lateinit var navController: NavController
+    private lateinit var navHostFragment: NavHostFragment
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            MyStoreTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
-    }
-}
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+        binding = MainContentBinding.inflate(layoutInflater)
+
+//        navHostFragment =
+//            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+//        navController = navHostFragment.navController
+
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     MyStoreTheme {
-        Greeting("Android")
+
     }
 }
